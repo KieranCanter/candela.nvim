b5d1a507e426fcd807e2c9873d13105beae128c5
Kieran <canter.kieran@gmail.com>
2025-07-21 01:38:41 -0400

    Cleanliness

--- lua/candela/commands.lua
+++ lua/candela/commands.lua
@@ -2,60 +2,68 @@
 local CandelaUi = require("candela.ui")
 
 local M = {}
-M.commands = {
-    ["add"] = function()
-        CandelaUi.show_patterns()
-        CandelaUi.add()
-    end,
-    ["edit"] = function()
-        CandelaUi.edit()
-    end,
-    ["copy"] = function()
-        CandelaUi.copy()
-    end,
-    ["delete"] = function()
-        CandelaUi.delete(true)
-    end,
-    ["clear"] = function()
-        CandelaUi.clear(true)
-    end,
-    ["refresh"] = function()
-        CandelaUi.refresh()
-    end,
-    ["change_color"] = function()
-        CandelaUi.change_color()
-    end,
-    ["toggle_highlight"] = function()
-        CandelaUi.toggle_highlight()
-    end,
-    ["toggle_lightbox"] = function()
-        CandelaUi.toggle_lightbox()
-    end,
-    ["match"] = function()
-        CandelaUi.match()
-    end,
-    ["find"] = function()
-        CandelaUi.find()
-        vim.api.nvim_cmd({ cmd = "lnext" }, {})
-        vim.api.nvim_cmd({ cmd = "lopen" }, {})
-    end,
-    ["find_all"] = function()
-        CandelaUi.find_all()
-        vim.api.nvim_cmd({ cmd = "lnext" }, {})
-        vim.api.nvim_cmd({ cmd = "lopen" }, {})
-    end,
-    ["help"] = function()
-        if vim.api.nvim_get_current_win() ~= CandelaUi.windows.regex.win then
-            vim.notify("Candela: must be in patterns window to see menu commands", vim.log.levels.ERROR)
-            return
-        end
-        vim.notify("Candela: help subcommand not implemented yet", vim.log.levels.WARN)
-        CandelaUi.help()
-    end,
-    ["health"] = function()
-        vim.cmd.checkhealth("candela")
-    end,
-}
+
+function M.setup(opts)
+    M.commands = {
+        add = function()
+            CandelaUi.show_patterns()
+            CandelaUi.add()
+        end,
+        edit = function()
+            CandelaUi.edit()
+        end,
+        copy = function()
+            CandelaUi.copy()
+        end,
+        delete = function()
+            local ask = opts.delete_confirmation
+            CandelaUi.delete(ask)
+        end,
+        clear = function()
+            local ask = opts.clear_confirmation
+            CandelaUi.clear(ask)
+        end,
+        refresh = function()
+            CandelaUi.refresh()
+        end,
+        change_color = function()
+            CandelaUi.change_color()
+        end,
+        toggle_highlight = function()
+            CandelaUi.toggle_highlight()
+        end,
+        toggle_lightbox = function()
+            CandelaUi.toggle_lightbox()
+        end,
+        match = function()
+            CandelaUi.match()
+        end,
+        match_all = function()
+            CandelaUi.match_all()
+        end,
+        find = function()
+            CandelaUi.find()
+            vim.api.nvim_cmd({ cmd = "lnext" }, {})
+            vim.api.nvim_cmd({ cmd = "lopen" }, {})
+        end,
+        find_all = function()
+            CandelaUi.find_all()
+            vim.api.nvim_cmd({ cmd = "lnext" }, {})
+            vim.api.nvim_cmd({ cmd = "lopen" }, {})
+        end,
+        help = function()
+            if vim.api.nvim_get_current_win() ~= CandelaUi.windows.regex.win then
+                vim.notify("Candela: must be in patterns window to see menu commands", vim.log.levels.ERROR)
+                return
+            end
+            vim.notify("Candela: help subcommand not implemented yet", vim.log.levels.WARN)
+            CandelaUi.help()
+        end,
+        health = function()
+            vim.cmd.checkhealth("candela")
+        end,
+    }
+end
 
 ---@param args table<string, any>
 function M.dispatch(args)
